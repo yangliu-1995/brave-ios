c9688e1fbea04530b6d9fb98e7dbbdc7060d396d
Soner Yuksel <grkmyksl@gmail.com>
2021-12-22 12:54:53 -0500

    Removing Downloads View Controller

--- Client/Frontend/Browser/BrowserNavigationHelper.swift
+++ Client/Frontend/Browser/BrowserNavigationHelper.swift
@@ -50,9 +50,7 @@ class BrowserNavigationHelper {
     }
     
     func openDownloads() {
-        guard let bvc = bvc else { return }
-        let vc = DownloadsPanel(profile: bvc.profile)
-        open(vc, doneButton: DoneButton(style: .done, position: .right))
+        FileManager.default.openBraveDownloadsFolder()
     }
     
     func openAddBookmark() {
